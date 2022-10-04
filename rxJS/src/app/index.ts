2f9d3cc1081d37c408c24b3555ca4822d5e390ed
Hayesito <juaeserna1@misena.edu.co>
2022-10-04 08:36:02 -0500

    Implementando Operators

--- rxJS/src/app/index.ts
+++ rxJS/src/app/index.ts
@@ -1,27 +1,33 @@
 import { Observable, Observer } from 'rxjs';
-
-const myObserver: Observer<any>{
-    next: x =>{
-        if(!isNaN(x)){
-            console.log(x + 10);
-        }else{
-            console.log(`'${x}': no es un numero`);
-        }
-    },
-    error: err => console.error(`Error`, err),
-    complete: () => console.log("solucionado")
+import { map, filter } from 'rxjs/operators';
+const myObserver: Observer<any> = {
+  next: (x) => {
+    if (!isNaN(x)) {
+      console.log(x + 10);
+    } else {
+      console.log(`'${x}': no es un numero`);
+    }
+  },
+  error: (err) => console.error(`ERROR`, err),
+  complete: () => console.log('mi trabajo aqui esta realizado'),
 };
 
-const myObservable = new Observable{
-    subscriber =>{
-        subscriber.next('1');
-    }
-};
+const myObservable = new Observable((subscriber) => {
+  subscriber.next('HOLA');
+  subscriber.next(10);
+  subscriber.next(20);
+  subscriber.error('error');
+});
+const myObservable2 = new Observable((subscriber) => {
+  subscriber.complete();
+});
 
-const myObservable2 = new Observable{
-    subscriber =>{;
-        subscriber.complete()
-    }
-}
+//myObservable.subscribe(myObserver);
+const mpipe = myObservable.pipe(
+  filter((r: any) => !isNaN(r)),
+  map((r: any) => {
+    return r + 10;
+  })
+);
 
-myObservable.subscribe(myObserver);
+mpipe.subscribe(myObserver);
