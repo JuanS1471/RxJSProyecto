5516bae98cf51579bd1b836d809b8ce0a5a9a0aa
Hayesito <juaeserna1@misena.edu.co>
2022-11-15 08:13:04 -0500

    Ajustes en codigo

--- rxJS/src/app/app.component.ts
+++ rxJS/src/app/app.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
-import { RxjsLabService } from './rxjs-lab.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxJS';
   constructor(
-    public rxjsService: RxjsLabService
   ){
 
   }
-
-
-  pipesFuncion(){
-    this.rxjsService.source.subscribe()
-  }
-  commonFuncion(){
-    this.rxjsService.myObservable.subscribe()
-  }
-  replayFuncion(){
-    this.rxjsService.source.subscribe()
-  }
-  behaviorFuncion(){
-    this.rxjsService.source.subscribe()
-  }
-
 }
